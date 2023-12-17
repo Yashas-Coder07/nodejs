391600374eabe3ffe009aab444c31c12605e083f
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-12-17 07:58:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const http = require('http');
 
 
 const server = http.createServer(function (req, res) {
-    console.log("Yashas");
+    const url = req.url;
+    if (url === "/home") {
+        res.write("<html>")
+        res.write("<head><title>Home Page</title></head>");
+        res.write("<body><h1>Welcome Home</h1></body>")
+        res.write("</html>")
+    }
+    else if (url === "/about") {
+        res.write("<html>")
+        res.write("<head><title>About Page</title></head>");
+        res.write("<body><h1>Welcome to About Us Page</h1></body>")
+        res.write("</html>")
+    }
+    else if (url === "/node") {
+        res.write("<html>")
+        res.write("<head><title>Welcome Page</title></head>");
+        res.write("<body><h1>Welcome to my Node Js project.</h1></body>")
+        res.write("</html>")
+    }
+    res.end();
 });
 
 server.listen(4000);
