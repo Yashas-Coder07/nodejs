b77d3ebfdcf7c7f287648929bb3b4c5a7a7fbaf6
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-12-17 11:26:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    fs.readFile('message.txt', (err, data) => {
+const routes = require('./routes')
 
-      res.setHeader('Content-Type', 'text/html');
-      console.log("data is " + data);
-      res.write('<html>');
-      res.write('<head><title>First Page</title><head>');
-      res.write(`<body>${data}</body>`);
-      res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-      );
-      res.write('</html>');
-      return res.end();
-    });
+const server = http.createServer(routes);
 
-  }
-  else if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', chunk => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, () => {
-        console.log(message);
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-});
-
-server.listen(4006);
+server.listen(4007);
