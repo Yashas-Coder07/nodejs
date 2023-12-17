22dbb660bba00bd85e2e5549076ee009a9475403
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-12-17 10:45:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const http = require('http');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    fs.readFile('message.txt', (err, data) => {
 
-const server = http.createServer(function (req, res) {
-    const url = req.url;
-    if (url === "/home") {
-        res.write("<html>")
-        res.write("<head><title>Home Page</title></head>");
-        res.write("<body><h1>Welcome Home</h1></body>")
-        res.write("</html>")
-    }
-    else if (url === "/about") {
-        res.write("<html>")
-        res.write("<head><title>About Page</title></head>");
-        res.write("<body><h1>Welcome to About Us Page</h1></body>")
-        res.write("</html>")
-    }
-    else if (url === "/node") {
-        res.write("<html>")
-        res.write("<head><title>Welcome Page</title></head>");
-        res.write("<body><h1>Welcome to my Node Js project.</h1></body>")
-        res.write("</html>")
-    }
-    res.end();
+      res.setHeader('Content-Type', 'text/html');
+      console.log("data is " + data);
+      res.write('<html>');
+      res.write('<head><title>First Page</title><head>');
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      res.write('</html>');
+      return res.end();
+    });
+
+  }
+  else if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, () => {
+        console.log(message);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+
 });
 
-server.listen(4000);
+server.listen(4006);
