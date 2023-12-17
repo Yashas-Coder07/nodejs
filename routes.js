0549efde373b1a60b3d38c4425ee0ab30ec55b48
Yashas-Coder <76394152+Yashas-Coder07@users.noreply.github.com>
2023-12-17 14:07:47 +0530

    Add files via upload

--- routes.js
+++ routes.js
@@ -27,7 +27,7 @@ function requestHandler(req, res) {
         });
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
+            const message = parsedBody.split('=')[0];
             fs.writeFile('message.txt', message, () => {
                 console.log(message);
                 res.statusCode = 302;
